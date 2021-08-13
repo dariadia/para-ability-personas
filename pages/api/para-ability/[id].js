901cd34a49a5f6e7e80f1def2ec1910472452b5a
Daria Diachkova <daria.diachkova@mail.ru>
2021-08-13 21:31:59 +0300

    feat(api): extend

--- pages/api/para-ability/[id].js
+++ pages/api/para-ability/[id].js
@@ -1,12 +1,16 @@
+import paraAbilities from '../para-abilities.json'
+
 export default function paraAbilityHandler(req, res) {
   const {
     query: { id },
     method,
   } = req
 
+  const paraAbility = paraAbilities.filter(item => item.id === id)[0]
+
   switch (method) {
     case 'GET':
-      res.status(200).json({ id, name: `para-ability ${id}` })
+      res.status(200).json({ ...paraAbility })
       break
     default:
       res.setHeader('Allow', ['GET'])
--- pages/api/persona/[id].js
+++ pages/api/persona/[id].js
@@ -1,12 +1,16 @@
+import personas from '../personas.json'
+
 export default function personaHandler(req, res) {
   const {
     query: { id },
     method,
   } = req
 
+  const persona = personas.filter(item => item.id === id)[0]
+
   switch (method) {
     case 'GET':
-      res.status(200).json({ id, name: `persona ${id}` })
+      res.status(200).json({ ...persona })
       break
     default:
       res.setHeader('Allow', ['GET'])
